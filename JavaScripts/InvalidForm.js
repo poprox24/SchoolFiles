fdee0fe58d63ed716b35e50e09081b431e3efee9
GamingRedCZ <hudec.maty.1995@gmail.com>
2021-02-09 17:41:19 +0100

    a

--- JavaScripts/InvalidForm.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("submitButton").addEventListener("click", function () {
-  document.getElementById("form").className = "submitted";
-});
-document.querySelector( "input" ).addEventListener( "invalid",
- function( event ) {
-        event.preventDefault();
-  });
