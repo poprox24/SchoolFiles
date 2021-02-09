b511b60b0bdcb8386b4be83e77bb7640dee3dbd7
GamingRedCZ <hudec.maty.1995@gmail.com>
2021-02-09 17:36:10 +0100

    a

--- Hidden/JavaScripts/Login.js
+++ Hidden/JavaScripts/Login.js
@@ -1,20 +1,19 @@
 var attempt = 2; // Variable to count number of attempts.
 // Below function Executes on click of login button.
 function login() {
-  var uname = "QUh1c2t5MjQ=";
-  var pword = "TVlTSVRFS3VyYXRlY2tvTVlTSVRFMTIzTVlTSVRF";
   var uname2 = "YWtybw==";
   var pword2 = "YWtybw==";
   var userinput = document.getElementById("id").value;
   var passinput = document.getElementById("password").value;
-  if (userinput == atob(uname) && passinput == atob(pword) && !q || userinput == atob(uname2) && passinput == atob(pword2) && q == atob("QWtybw==")) {
+
+  if (userinput == atob(uname2) && passinput == atob(pword2) && q == atob("QWtybw==")) {
     alert("Login Succesful");
     loadPage(); // Redirecting to other page.
     return false;
   } else {
-    attempt--; // Decrementing by one.
+    attempt--;
     alert("You have left " + attempt + " attempt;");
-    // Disabling fields after 3 attempts.
+
     if (attempt == 0) {
       document.getElementById("id").disabled = true;
       document.getElementById("password").disabled = true;
--- Hidden/JavaScripts/View.js
+++ Hidden/JavaScripts/View.js
@@ -1,12 +1,17 @@
 const queryString = window.location.search;
 const q = new URLSearchParams(queryString).get("q");
 
+window.onload = function () {
+  if (!q) {
+    load("QWZ0ZXJsb2dpbi5odG1s");
+  }
+}
+
 function loadPage() {
   if (q == atob("QWtybw==")) {
     load("QWtyby5odG1s");
-  } else {
-    $("#loader").load(atob("QWZ0ZXJsb2dpbi5odG1s"));
   }
+  
   var url = document.location.href;
   window.history.pushState({}, "", url.split("?")[0]);
 };
