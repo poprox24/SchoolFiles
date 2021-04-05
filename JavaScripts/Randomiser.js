12c3b1a853a0c9742993a9a0a3341752f57b6515
AkaHusky24 <hudec.maty.1995@gmail.com>
2021-04-05 15:25:28 +0200

    Who knows

--- JavaScripts/Randomiser.js
+++ JavaScripts/Randomiser.js
@@ -1,6 +1,6 @@
 var randomFromList = [
     'https://bit.ly/33CR0Yp',
-    'https://bit.ly/3ceDZbp',
+    'https://bit.ly/3rRl2lh',
     'https://bit.ly/33Eqhe7',
     'https://bit.ly/2REfLxO'
   ]
@@ -10,12 +10,12 @@ var randomFromList = [
   document.getElementById("randomIMG").src = randomFromList[randomiser];
 
   var randomFromList = [
-    'https://bit.ly/33CR0Yp',
-    'https://bit.ly/3ceDZbp',
-    'https://bit.ly/33Eqhe7',
-    'https://bit.ly/2REfLxO'
+    'https://bit.ly/2ZOdLHS',
+    'https://bit.ly/2FIQ78T',
+    'https://bit.ly/3mvfiMF',
+    'https://bit.ly/2EaFHho'
   ]
 
   var randomiser = Math.floor(Math.random()*randomFromList.length);
 
-  document.getElementById("randomCustomIMG").src = randomFromList[randomiser];
+  document.getElementById("randomcustomIMG").src = randomFromList[randomiser];
